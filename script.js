ec760365e5be428466cce5d9e29640765110b9fa
Heavenpatel5114 <113304844+Heavenpatel5114@users.noreply.github.com>
2023-03-21 08:20:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
+	let s=0,e=str.length-1;
 
+	str=str.toLowercase();
+	while(s<=e)
+		{
+			if(str.CharAt(s)===str.charAt(e))
+			{s++;e--;}
+
+			else
+				return false;
+		}
+	return true;
 }
 module.exports = palindrome
